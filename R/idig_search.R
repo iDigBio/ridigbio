6f92268527100d4e26f59a87db8dbcee54764014
Matthew Collins <mcollins@acis.ufl.edu>
2014-11-07 15:45:17 -0500

    Working on looping to retrieve all results, not finished

--- R/idig_search.R
+++ R/idig_search.R
@@ -19,13 +19,44 @@ idig_search <- function(query, limit=100, offset=0, fields=c("dwc:catalogNumber"
                                    "dwc:decimalLongitude"), ...) {
     stopifnot(inherits(query, "list"))
     query <- list(limit=limit, offset=offset, rq=query)
-    search_results <- idig_POST("search", body=query)
-    search_items <- httr::content(search_results)$items
-    lst_data_full <- lapply(search_items, function(x) x$data)
-    lst_data <- lapply(lst_data_full, function(x, keep=fields) {
-        unlist(x[keep])[keep]
-    })
-    dat <- data.frame(do.call("rbind", lst_data))
-    names(dat) <- fields
+
+    max_items <- 7
+    
+    items <- -1
+    item_count <- 0
+    dat <- FALSE    
+    
+    while (items < item_count && items < max_items){
+      search_results <- idig_POST("search", body=query)
+      
+      # Slight possibility of the number of items changing as we go due to inserts/
+      # deletes at iDigBio, keep it current
+      item_count <- fmt_search_txt_to_itemCount(search_results)
+      
+      if (inherits(dat, "logical")){
+        dat <- fmt_search_txt_to_df(search_results, fields)
+#      } else {
+#        dat <- do.call(rbind, list(df, fmt_search_txt_to_df(search_results, fields)))
+      }
+      items <- items + nrow(dat)
+      query$offset <- items
+    }
+    
     dat
 }
+
+fmt_search_txt_to_itemCount <- function(txt){
+  httr::content(txt)$itemCount
+}
+
+fmt_search_txt_to_df <- function(txt, fields) {
+  search_items <- httr::content(txt)$items
+  lst_data_full <- lapply(search_items, function(x) x$data)
+  lst_data <- lapply(lst_data_full, function(x, keep=fields) {
+    unlist(x[keep])[keep]
+  })
+  dat <- data.frame(do.call("rbind", lst_data))
+  names(dat) <- fields
+  dat
+}
+
