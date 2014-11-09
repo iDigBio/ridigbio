bd701d3084fcbb815739483d928e39cec74cec35
Matthew Collins <mcollins@acis.ufl.edu>
2014-11-09 17:01:09 -0500

    More work experimenting with offset

--- R/idig_search.R
+++ R/idig_search.R
@@ -4,14 +4,14 @@
 ##' returned up to the max_items cap (default 100,000). If more results are 
 ##' wanted, the max_items can be passed as an option. Limit and offset are
 ##' availible if manual paging of results is needed though the max_items cap
-##' still applies as the record count comes from the results header not the 
+##' still applies as the item count comes from the results header not the 
 ##' count of actual records in the limit/offset window.
 ##' @title Basic searching of the iDigBio API
 ##' @param query a list containing the information to be searched in iDigBio
 ##' @param fields list of fields that will be contained in the data.frame
 ##' @param max_items maximum number of items to attempt to retrieve
 ##' @param limit maximum number of records to request
-##' @param offset where to search the search
+##' @param offset where to start the search
 ##' @param ... additional parameters (currently not in use)
 ##' @return a data frame
 ##' @author Francois Michonneau
@@ -20,32 +20,44 @@
 ##' idig_search(query=list(genus="acer"))
 ##' }
 ##' @export
-idig_search <- function(query, fields=c("dwc:catalogNumber", "dwc:genus",
+idig_search <- function(idig_query, fields=c("dwc:catalogNumber", "dwc:genus",
                                         "dwc:specificEpithet", "dwc:decimalLatitude",
                                         "dwc:decimalLongitude"), 
-                        max_items=100000, limit=5000, offset=0, ...) {
-    stopifnot(inherits(query, "list"))
-    query <- list(limit=limit, offset=offset, rq=query)
+                        max_items=100000, limit=0, offset=0, ...) {
+  
+    stopifnot(inherits(idig_query, "list") && length(idig_query) > 0)
+    query <- list(rq=idig_query, offset=offset)
+    if (limit > 0){
+      query$limit <- limit
+    }else{
+      query$limit <- 5000 # as of 11/9/14 this is max records allowed at once
+    }
    
-    items <- 0
+    dat <- data.frame()
     item_count <- 1 # trick to get inside loop first time
-    dat <- FALSE    # flag value to make first assignment in loop
     
-    while (items < item_count && items < max_items){
+    # loop until we either have all results or all results the user wants
+    while (nrow(dat) < item_count){# && (limit == 0 || nrow(dat) > limit)){
       search_results <- idig_POST("search", body=query)
       
       # Slight possibility of the number of items changing as we go due to inserts/
-      # deletes at iDigBio, put this inside the loop
+      # deletes at iDigBio, put this inside the loop to keep it current
       item_count <- fmt_search_txt_to_itemCount(search_results)
-      stopifnot(item_count < max_items)
+      if (item_count > max_items){
+        stop(paste0("Search would return more than ", max_items, 
+                    " results. See max_items argument."))
+      }
       
-      if (inherits(dat, "logical")){
+      if (nrow(dat) == 0){
         dat <- fmt_search_txt_to_df(search_results, fields)
       } else {
         dat <- do.call(rbind, list(dat, fmt_search_txt_to_df(search_results, fields)))
       }
-      items <- nrow(dat)
-      query$offset <- items
+      
+      query$offset <- nrow(dat)
+      #if (limit > 0){
+      #  query$limit <- nrow(dat) %% limit
+      #}
     }
     
     dat
