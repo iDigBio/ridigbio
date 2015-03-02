742049d37b04b1c940091ca6a749c00303af3ac5
Matthew Collins <mcollins@acis.ufl.edu>
2015-03-02 11:06:17 -0500

    Updated tests to idig_top_records

--- tests/testthat/test-idig_top_records.R
+++ tests/testthat/test-idig_top_records.R
@@ -7,7 +7,7 @@ genus <- "acer"
 scientificname <- "acer macrophyllum"
 
 # Default list of top 10 scientific names
-top <- idig_toprecords()
+top <- idig_top_records()
 expect_that(top, is_a("list"))
 expect_that(length(top$scientificname), equals(10))
 expect_that(top$itemCount > 20 * 1000 * 1000, is_true())
@@ -15,7 +15,7 @@ expect_that(top$itemCount > 20 * 1000 * 1000, is_true())
 all_count <- top$itemCount
 
 # Field and number of tops
-top <- idig_toprecords(top_fields=c(field), count=count)
+top <- idig_top_records(top_fields=c(field), count=count)
 expect_that(top, is_a("list"))
 expect_that(length(top[[field]]), equals(count))
 expect_that(top[[field]][[most]][["itemCount"]] > 1000 * 1000, is_true())
@@ -23,7 +23,7 @@ expect_that(top[[field]][[most]][["itemCount"]] > 1000 * 1000, is_true())
 expect_that(abs(top$itemCount - all_count) < 1000, is_true())
 
 # Searches
-top <- idig_toprecords(rq=list("genus"=genus), top_fields=c(field),
+top <- idig_top_records(rq=list("genus"=genus), top_fields=c(field),
                        count=count)
 expect_that(top, is_a("list"))
 expect_that(top$itemCount < 200 * 1000, is_true())
@@ -31,7 +31,7 @@ expect_that(top$itemCount < 200 * 1000, is_true())
 genus_count <- top$itemCount
 
 # Multiple fields
-top <- idig_toprecords(rq=list("genus"=genus), top_fields=c(field, 
+top <- idig_top_records(rq=list("genus"=genus), top_fields=c(field, 
                        "scientificname"), count=count)
 expect_that(top, is_a("list"))
 expect_that(abs(top$itemCount - genus_count) < 100, is_true())
