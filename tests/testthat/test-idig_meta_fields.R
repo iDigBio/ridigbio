904123b9f0b7a6eaf326c86379a30b4cd62a43ba
Matthew Collins <mcollins@acis.ufl.edu>
2015-03-02 15:54:11 -0500

    Changed mediarecords to media to make more standard

--- tests/testthat/test-idig_meta_fields.R
+++ tests/testthat/test-idig_meta_fields.R
@@ -7,7 +7,7 @@ expect_that(f[["data"]], is_a("list"))
 expect_that(f[["uuid"]][["type"]] == "string", is_true())
 
 # Media
-f <- idig_meta_fields(type="mediarecords")
+f <- idig_meta_fields(type="media")
 expect_that(f, is_a("list"))
 expect_that(f[["data"]], is_a("list"))
 expect_that(f[["uuid"]][["type"]] == "string", is_true())
