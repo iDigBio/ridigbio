fd3f4b1871ad86500cb9068a3372a37712b22aa3
Matthew Collins <mcollins@acis.ufl.edu>
2016-10-25 23:57:49 -0400

    Added to idig_buid_attrib documentation and fixed up formatting a bit to match.

--- tests/testthat/test-idig_build_attrib.R
+++ tests/testthat/test-idig_build_attrib.R
@@ -1,9 +1,9 @@
 context("test idig_build_attrib")
 
-testQuery <- idig_search_records(rq=list(family="holothuriidae"))
-
 test_that("attribution dataframe built from results dataframe and item counts match", {
   testthat::skip_on_cran()
+
+  testQuery <- idig_search_records(rq=list(family="holothuriidae"))
   df <- idig_build_attrib(testQuery)
   expect_that(df, is_a("data.frame"))
   expect_that(sum(df$itemCount) == attributes(testQuery)$itemCount, is_true())
@@ -11,9 +11,10 @@ test_that("attribution dataframe built from results dataframe and item counts ma
 })
 
 test_that("limited results, attribution counts match limit and result counts", {
-        testthat::skip_on_cran()
-        df <- idig_search_records(rq=list(genus="acer"), limit=10)
-        tt <- idig_build_attrib(df)
-        expect_that(nrow(df) == 10, is_true())
-        expect_that(sum(tt$itemCount) == 10, is_true())
+  testthat::skip_on_cran()
+
+  df <- idig_search_records(rq=list(genus="acer"), limit=10)
+  tt <- idig_build_attrib(df)
+  expect_that(nrow(df) == 10, is_true())
+  expect_that(sum(tt$itemCount) == 10, is_true())
 })
